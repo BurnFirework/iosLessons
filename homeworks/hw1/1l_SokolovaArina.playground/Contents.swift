5522956445fb65e424b49e9acf63ae8a65f52d19
Arina Sokolova <arinylka2003@gmail.com>
2022-03-30 11:57:40 +0300

    fixed style errors

--- homeworks/hw1/1l_SokolovaArina.playground/Contents.swift
+++ homeworks/hw1/1l_SokolovaArina.playground/Contents.swift
@@ -1,26 +1,29 @@
-import Darwin
+import Foundation
 
 //1
-let a: Double = 1, b: Double = 10, c: Double = -40 //data
+let a: Double = 1
+let b: Double = 10 
+let c: Double = -40 //data
 let discriminant = b * b - 4 * a * c
 if (discriminant > 0 && a != 0) {
     print((-b + sqrt(discriminant)) / (2 * a), (-b - sqrt(discriminant)) / (2 * a))
-}
-else if (discriminant == 0 && a != 0) {
+} else if (discriminant == 0 && a != 0) {
     print(-b / (2 * a))
-}
-else {
+} else {
     print("Error")
 }
 
 //2
-let cathet1: Double = 5, cathet2: Double = 10 //data
-let hypotenuse = sqrt(cathet2 * cathet2 + cathet1 * cathet1), perimeter = cathet1 + cathet2 + hypotenuse
+let cathet1: Double = 5
+let cathet2: Double = 10 //data
+let hypotenuse = sqrt(cathet2 * cathet2 + cathet1 * cathet1)
+let perimeter = cathet1 + cathet2 + hypotenuse
 print("Square = \(0.5 * cathet1 * cathet2)")
 print("Hypotenuse = \(hypotenuse)")
 print("Perimeter = \(perimeter)")
 
 //3
-let summa: Double = 10000, percent: Double = 5 //data
+let summa: Double = 10000
+let percent: Double = 5 //data
 let increase = 1.0 + percent / 100
-print(summa * increase * increase * increase * increase * increase)
+print(summa * pow(increase, 5))
